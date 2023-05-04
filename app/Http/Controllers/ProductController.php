23258dab2f8cf5cba9f9e66a2b714c198805fb60
Ahmed Eltaweel <ahmed.taweel@westore.ai>
2023-05-04 20:07:40 +0300

    add-scope-active-products-to-get-only-products-that-available-to-order

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 class ProductController extends Controller
 {
     public function index(Request $request){
-        return ProductResource::collection(Product::filter($request->only(
+        return ProductResource::collection(Product::active()->filter($request->only(
             ['name','id','price_from','price_to','in_ingredients']
         ))->with('orders')->paginate($request->get('perPage')??20));
     }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use App\ModelFilters\ProductFilter;
 use EloquentFilter\Filterable;
+use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -30,5 +31,14 @@ class Product extends Model
     public function modelFilter(): string
     {
         return $this->provideFilter(ProductFilter::class);
+    }
+    public function scopeActive(Builder $query)
+    {
+        $query->raw("SELECT * FROM `products`, `ingredient_product`, `ingredients`
+            WHERE EXISTS(
+                SELECT * FROM `ingredients`
+                    INNER JOIN `ingredient_product` ON `ingredients`.`id` = `ingredient_product`.`ingredient_id`
+                    WHERE `products`.`id` = `ingredient_product`.`product_id`
+                        ) AND `ingredient_product`.`weight` <= `ingredients`.`stock`");
     }
 }
